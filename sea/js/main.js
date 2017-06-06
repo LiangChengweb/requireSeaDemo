ec9b8443a57a63c2c0094320cafaa91e59837df6
liangcheng <1352159038@qq.com>
2017-06-06 09:49:18 +0800

    sea init

--- sea/js/main.js
+++ sea/js/main.js
@@ -1,26 +1,3 @@
-/*
- require.config({
- baseUrl: 'js',
- paths: {
- "jquery" : "jquery."
- }
- })
- */
-require.config({
-    paths: {
-        "jquery": "lib/jquery"
-    },
-    shim: {
-        "add": {
-            exports: 'jquery.fn.add',
-            deps: ['jquery']
-        }
-    }
+define(function(){
+    alert('1111');
 });
-require(['jquery', 'math', 'green',"jquery.fn.add",], function ($, math, green) {
-    $(function () {
-        $('.box').add(1);
-        green.toGreen();
-        alert(math.add(1, 1));
-    })
-})
